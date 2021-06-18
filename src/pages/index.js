e03f696cd05a3bd89a88798faac3afc00e9ca465
basimkarhabi <basim.karhabi@gmail.com>
2021-06-18 11:05:21 +0200

    Finished Gatsby Tutorial Oart 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="Home Page1">
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   )
